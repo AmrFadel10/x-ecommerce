6485a7509861b511ca0eeed6be20c4d6ee774af2
Amr Fadel <afadel1310@gmail.com>
2024-12-02 21:25:27 +0200

    fix some bugs

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const request = axios.create({
-  baseURL: "http://localhost:8000/api/v2",
+  baseURL: "https://af-commerce-backend.vercel.app//api/v2",
 });
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://af-commerce-backend.vercel.app/";
 export default request;
